979f6f97faa05956f52cb06962a25f4b1b070783
syeda <syedaaisha7786@gmail.com>
2023-11-11 23:08:13 -0600

    mock-call-tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// src//App.js
+import React, { useState } from 'react';
+import MockCallList from './MockCallList';
+import AddMockCall from './AddMockCall';
 
 function App() {
+  const [mockCalls, setMockCalls] = useState([]);
+  const [editingCall, setEditingCall] = useState(null);
+
+  const addCall = (newCall) => {
+    setMockCalls([...mockCalls, newCall]);
+  };
+
+  const editCall = (index) => {
+    setEditingCall({ ...mockCalls[index], index });
+  };
+
+  const updateCall = (index, updatedCall) => {
+    const updatedCalls = [...mockCalls];
+    updatedCalls[index] = updatedCall;
+    setMockCalls(updatedCalls);
+    setEditingCall(null);
+  };
+
+  const deleteCall = (index) => {
+    const updatedCalls = mockCalls.filter((_, i) => i !== index);
+    setMockCalls(updatedCalls);
+    setEditingCall(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <h1>Mock Call Monitoring App</h1>
+      <AddMockCall
+        addCall={addCall}
+        editingCall={editingCall}
+        updateCall={updateCall}
+      />
+      <MockCallList
+        calls={mockCalls}
+        editCall={editCall}
+        deleteCall={deleteCall}
+      />
     </div>
+
   );
 }
 
